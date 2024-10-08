8710ce609186c2b7246dc475b60b8aeceb7a73da
小林　航大 <kodai.kobayashi@drecom.co.jp>
2024-10-08 18:52:44 +0900

    activerecordの定数を再定義する

--- lib/active_record/turntable/active_record_ext/association_builder.rb
+++ lib/active_record/turntable/active_record_ext/association_builder.rb
@@ -3,6 +3,10 @@ require "active_record/associations/builder/association"
 module ActiveRecord::Turntable
   module ActiveRecordExt
     module AssociationBuilder
+      if ActiveRecord::Associations::Builder::Association.const_defined?(:VALID_OPTIONS)
+        ActiveRecord::Associations::Builder::Association.send(:remove_const, :VALID_OPTIONS)
+      end
+
       ActiveRecord::Associations::Builder::Association::VALID_OPTIONS = [
         :class_name, :anonymous_class, :primary_key, :foreign_key, :dependent, :validate, :inverse_of, :strict_loading, :foreign_shard_key
       ].freeze
