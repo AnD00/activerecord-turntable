32acc3f1a590e09837279835f80f8094d4ae2e81
haraki-tbn <masaya_haraki@tbn.co.jp>
2025-07-01 12:32:46 +0900

    support ruby3.2

--- activerecord-turntable.gemspec
+++ activerecord-turntable.gemspec
@@ -60,5 +60,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bcrypt", "~> 3.1.11"
   spec.add_development_dependency "minitest"
   spec.add_development_dependency "mocha"
-  spec.add_development_dependency "sqlite3", "~> 1.3.6"
+  spec.add_development_dependency "sqlite3"
 end
--- lib/active_record/turntable/connection_proxy.rb
+++ lib/active_record/turntable/connection_proxy.rb
@@ -33,7 +33,7 @@ module ActiveRecord::Turntable
 
     def transaction(options = {}, &block)
       with_master {
-        connection.transaction(options, &block)
+        connection.transaction(**options, &block)
       }
     end
 
@@ -88,7 +88,9 @@ module ActiveRecord::Turntable
     def method_missing(method, *args, &block)
       clear_query_cache_if_needed(method)
       if shard_fixed?
-        connection.send(method, *args, &block)
+        hashes, positional_args = args.partition { |arg| arg.is_a?(Hash) }
+        kwargs = hashes.reduce({}, :merge)
+        connection.send(method, *positional_args, **kwargs, &block)
       elsif mixable?(method, *args)
         fader = @mixer.build_fader(method, *args, &block)
         logger.debug {
